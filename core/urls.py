01c068e20b75ce2081c1202f83fc57528c063577
nila <nilabmd2@gmail.com>
2026-07-11 16:14:56 +0330

    inter viwer for home page

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('welcome to the dashboard')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls'))
+    path('',home),
+    path('blog/',include('blog.urls')),
+
 ]
